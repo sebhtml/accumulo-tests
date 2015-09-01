9da358918249476b2499678c2d91eddd15f27e77
Sebastien Boisvert <boisvert@anl.gov>
2015-09-01 21:19:49 +0000

    example: add word count example

---
+++ map-reduce-tutorial/WordCounter.java
@@ -0,0 +1,72 @@
+package gov.anl.testseb;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+public class WordCounter {
+
+    public static class MyMapClass extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+
+            while (tokenizer.hasMoreTokens()) {
+
+                String wordInLine = tokenizer.nextToken();
+                word.set(wordInLine);
+                output.collect(word, one);
+            }
+        }
+    }
+
+    public static class MyReduceClass extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output,
+                Reporter reporter) throws IOException {
+
+            int sum = 0;
+
+            while (values.hasNext()) {
+
+                IntWritable value = values.next();
+                int count = value.get();
+                sum += count;
+            }
+
+            IntWritable count = new IntWritable(sum);
+            output.collect(key, count);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        JobConf conf = new JobConf(WordCounter.class);
+
+        conf.setJobName("WordCounter");
+
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+
+        conf.setMapperClass(MyMapClass.class);
+        conf.setCombinerClass(MyReduceClass.class);
+        conf.setReducerClass(MyReduceClass.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+        JobClient.runJob(conf);
+    }
+}
