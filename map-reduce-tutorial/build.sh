784cbe425a6da3bb251dc34c3476e93b36268afb
Sebastien Boisvert <boisvert@anl.gov>
2015-09-01 21:59:41 +0000

    use a build script to generate the jar file

---
+++ map-reduce-tutorial/build.sh
@@ -0,0 +1,15 @@
+
+# Needs Hadoop 2.3.0-cdh5.1.0
+
+mkdir -p classes
+#javac \
+#    -cp $HADOOP_HOME/client-0.20/hadoop-hdfs-2.3.0-cdh5.1.0.jar \
+#    -cp $HADOOP_HOME/client-0.20/hadoop-core-2.3.0-mr1-cdh5.1.0.jar \
+#    -cp $HADOOP_HOME/hadoop-common-2.3.0-cdh5.1.0.jar \
+#    -d classes WordCounter.java
+
+javac \
+    -classpath `hadoop classpath` \
+    -d classes WordCounter.java
+
+jar -cvf wordcounter.jar -C classes
